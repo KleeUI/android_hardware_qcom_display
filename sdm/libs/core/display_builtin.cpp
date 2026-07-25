6c1af0483c22d7c51db60208ee7354fb330eb47c
herobrinejjjj <herobrinejjj@gmail.com>
2026-07-25 17:15:33 +0000

    sdm: Fix AOSP 17 format diagnostics
    Change-Id: Iec3a4190751f60844413db1e147ee675e2bb915f

--- sdm/libs/core/display_builtin.cpp
+++ sdm/libs/core/display_builtin.cpp
@@ -36,6 +36,7 @@
 #include <utils/formats.h>
 #include <core/buffer_allocator.h>
 #include <core/core_interface.h>
+#include <inttypes.h>
 #include <sys/mman.h>
 #include <private/hw_interface.h>
 #include <private/hw_info_interface.h>
@@ -565,7 +566,7 @@ DisplayError DisplayBuiltIn::SetupDemura() {
   hfc_buffer_size_ = buf_out_params->buffers[0].size;
 #endif
   input_cfg.panel_id = panel_id_;
-  DLOGI("panel id %lx\n", input_cfg.panel_id);
+  DLOGI("panel id %" PRIx64 "\n", input_cfg.panel_id);
   std::unique_ptr<DemuraIntf> demura =
       pf_factory_->CreateDemuraIntf(input_cfg, prop_intf_, buffer_allocator_, spr_);
   if (!demura) {
@@ -716,14 +717,14 @@ DisplayError DisplayBuiltIn::SetupDemuraT0AndTn() {
     Debug::Get()->GetProperty(DEMURA_PRIMARY_PANEL_OVERRIDE_HIGH, &panel_id_w);
     panel_id |= ((static_cast<uint64_t>(panel_id_w)) << 32);
     Debug::Get()->GetProperty(DISABLE_DEMURA_PRIMARY, &value);
-    DLOGI("panel overide total value %lx\n", panel_id);
+    DLOGI("panel overide total value %" PRIx64 "\n", panel_id);
   } else {
     Debug::Get()->GetProperty(DEMURA_SECONDARY_PANEL_OVERRIDE_LOW, &panel_id_w);
     panel_id = static_cast<uint32_t>(panel_id_w);
     Debug::Get()->GetProperty(DEMURA_SECONDARY_PANEL_OVERRIDE_HIGH, &panel_id_w);
     panel_id |= ((static_cast<uint64_t>(panel_id_w)) << 32);
     Debug::Get()->GetProperty(DISABLE_DEMURA_SECONDARY, &value);
-    DLOGI("panel overide total value %lx\n", panel_id);
+    DLOGI("panel overide total value %" PRIx64 "\n", panel_id);
   }
 
   if (value > 0) {
@@ -745,7 +746,7 @@ DisplayError DisplayBuiltIn::SetupDemuraT0AndTn() {
     }
   }
   panel_id_ = panel_id;
-  DLOGI("panel_id 0x%lx", panel_id_);
+  DLOGI("panel_id 0x%" PRIx64, panel_id_);
 
 #if defined SDM_UNIT_TESTING || defined TRUSTED_VM
   demura_allowed = true;
@@ -2845,8 +2846,9 @@ void DisplayIPCVmCallbackImpl::ExportHFCBuffer() {
   export_buf_in_params->panel_id = panel_id_;
   export_buf_in_params->mem_handle = buffer_info_hfc_.alloc_buffer_info.mem_handle;
 
-  DLOGI("Allocated hfc buffer mem_handle %d size %d panel id :%x", export_buf_in_params->mem_handle,
-        export_buf_in_params->size, export_buf_in_params->panel_id);
+  DLOGI("Allocated hfc buffer mem_handle %" PRId64 " size %d panel id :%" PRIx64,
+        export_buf_in_params->mem_handle, export_buf_in_params->size,
+        export_buf_in_params->panel_id);
   if ((ret = ipc_intf_->SetParameter(kIpcParamSetHFCBuffer, in))) {
     DLOGE("Failed to export demura buffers, error = %d", ret);
     buffer_allocator_->FreeBuffer(&buffer_info_hfc_);
